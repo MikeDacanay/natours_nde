db332a44a3ba6b46a76581d00b8896c54fdc1d21
Mike Dacanay <migd64@gmail.com>
2021-08-06 01:05:18 -0700

    Tour Resource: Model, ctrler created

--- server.js
+++ server.js
@@ -15,25 +15,8 @@ mongoose
         console.log('DB connection successful!');
     });
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
 const port = 3000;
+
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 });
