6b1d5ed050dcfd7270e317e310d19bab950ddab5
Mike Dacanay <migd64@gmail.com>
2021-08-10 00:04:32 -0700

    added model.schema basic validators

--- models/tourModels.js
+++ models/tourModels.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have lte then 40 chars'],
+        minlength: [10, 'A tour name must be gte then 10'],
     },
     slug: String,
     duration: {
@@ -19,11 +21,15 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficult is eather: easy, medium, difficult',
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
     },
     ratingsQuantity: {
         type: Number,
@@ -31,7 +37,9 @@ const tourSchema = new mongoose.Schema({
     },
     rating: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1'],
+        max: [5, 'Rating must be below 5']        
     },
     price: {
         type: Number,
@@ -57,7 +65,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -72,7 +84,29 @@ tourSchema.pre('save', function(next){
     this.slug = slugify(this.name, {lower: true});
     next();
 });
-    //save doesnt workign find and update
+    //save doesnt workign findOne andupdate
+
+// QUERY MIDDLEWARE
+tourSchema.pre( /^find/, function(next) {
+    this.find({ secretTour: { $ne: true }})
+    
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    // console.log(`Query Took ${Date.now() - this.start} milliseconds`);
+    // console.log(docs);
+    next();
+})
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next){
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true}}})
+
+    console.log(this.pipeline());
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
